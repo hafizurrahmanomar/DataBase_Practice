3ee7f01a35606e1ef62c76cee15fe882df0dbf9b
hafizurrahmanomar <hafizurrahmanomar@gmail.com>
2024-12-11 18:17:32 +0600

    dataBae File save

--- eCommerce_Database/Basic_query.sql
+++ eCommerce_Database/Basic_query.sql
@@ -13,12 +13,29 @@ SELECT *FROM products;
 -- simple Column Select
 SELECT name,price FROM products;
 
+-- simple select query using Alias( At First => SELECT FROM products p; than . use)
+SELECT p.name, p.price, p.stock FROM products p;
+SELECT c.id, c.name FROM categories c;
 
- 
+ -- Join Query users->u categories->c must be relationship
+SELECT p.name, p.description, p.updated_at, c.name,c.created_at FROM products p INNER JOIN categories c ON p.category_id=c.id;
+SELECT p.name, p.description, p.updated_at, c.name,c.created_at FROM products p LEFT JOIN categories c ON p.category_id=c.id;
+SELECT p.name, p.description, p.updated_at, c.name,c.created_at FROM products p RIGHT JOIN categories c ON p.category_id=c.id;
+SELECT p.name, p.description, p.updated_at, c.name,c.created_at FROM products p CROSS JOIN categories c ON p.category_id=c.id;
+
 -- Filtering Data
 SELECT * FROM products WHERE price > 1900;
+SELECT * FROM products WHERE name="MacBook Mini 4 Pro";
+
+-- Data Delete
+DELETE FROM products WHERE id=20;
+
+-- Data Update
+UPDATE products SET name ='iPhone 16 pro', description= 'Good Phone', price ='180000', stock ='50' WHERE id = 1;
+
+-- Data Insert
+INSERT INTO products (name, description, price, stock, category_id, image_url) VALUES ('MacBook Mini 4 Pro', 'Apple Mini for professionals', 76000, 30, 1, 'macbook_mini4.jpg');
 
 SET SQL_SAFE_UPDATES = 0;
 
--- simple select query using Alias
-SELECT a.name, a.price, a.stock, FROM products a
+
--- sqlLanguage.sql
+++ sqlLanguage.sql
@@ -8,6 +8,8 @@ SELECT ("Hello World");
 SELECT  'Hello world!' as mag;
 
 SELECT CONCAT('Hello ', 'World!') AS Concatenation;
+-- World! Replace to MYSQL
+SELECT REPLACE('Hello World!', 'World!', 'MYSQL') AS Stri_Replace;
 
 SELECT CONCAT_WS(' ', 'Hello', 'World!') AS Concatenation_With_Separator;
 
@@ -37,6 +39,15 @@ SELECT
 @num1 / @num2 AS Division,
 @num1 % @num2 AS Modulus;
 
+-- Random Value Create
+SELECT RAND() RandomValue;
+SELECT FLOOR(RAND() * 100) AS RandomNumber;
+SELECT LEAST(@num1,@num2) As Min_Value;
+SELECT GREATEST(@num1,@num2) As Max_Value;
+SELECT PI() As PI_Value;
+SELECT FORMAT(102578000.00045,2) as COL;
+
+
 -- power,square root,Round value
 
 
@@ -52,15 +63,29 @@ SELECT
 SELECT LOWER("Hafizur Rahman") AS Lower_Case;
 SELECT REVERSE("Hafizur Rahman") AS Reverse_String;
 SELECT UPPER("Hafizur Rahman") AS Upper_String;
+SELECT TRIM("              Hafizur Rahman             ") AS Triming_String;
+SELECT LTRIM("              Hafizur Rahman             ") AS L_Triming_String;
+SELECT RTRIM("              Hafizur Rahman             ") AS R_Triming_String;
 SELECT SIN(30) AS Sin_30;
 SELECT COS(30) AS COS_30;
-SELECT Now() as CurrentTime;
+-- Date And Time
+SELECT Now() as CurrentDateAndTime;
+--Date
+SELECT CURDATE() as CurrentDate;
+SELECT CURTIME() as CurrentTime;
+SELECT CURRENT_TIME() as CurrentTime;
+
+-- Current Time+ 1 Day
 SELECT DATE_ADD(CURDATE(), INTERVAL 1 DAY) AS Tomorrow;
+-- Current Time - 1 Day
 SELECT DATE_SUB(CURDATE(), INTERVAL 1 DAY) AS Yesterday;
+-- Date Deferance
 SELECT DATEDIFF('2024-12-10','1986-12-30') as DateOfBirth;
+-- minute Slice
 SELECT MINUTE('2024-12-10 13:59:23') as Min;
+-- second Slice
 SELECT SECOND('2024-12-10 13:59:23') as SEC;
+-- Year Slice
 SELECT YEAR('2024-12-10 13:59:23') as Yea;
 
--- 4. Aggregate Function
--- count(),avg(),min(),max(),
+
